879f9450681eaf17f4fca9b17434928f439a33ee
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2025-02-26 02:19:35 +0300

    feat: init services structure

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,26 @@
 package service
+
+import (
+	"github.com/EtoNeAnanasbI95/test-task/internal/repository"
+	"log/slog"
+)
+
+type Songs interface {
+	Create() (int, error)
+	Get(id int) error
+	GetAll() error
+	Update(id int) error
+	Delete(id int) error
+}
+
+type Service struct {
+	log *slog.Logger
+	Songs
+}
+
+func NewService(l *slog.Logger, r *repository.Repository) *Service {
+	return &Service{
+		log: l,
+		//Songs: ,
+	}
+}
