edc0cc333a566c4829c2dc19bb68147d5a70338d
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2025-02-26 02:18:51 +0300

    feat: init hanldlers

--- internal/handler/hanlder.go
+++ internal/handler/hanlder.go
@@ -1,1 +1,36 @@
 package handler
+
+import (
+	"github.com/EtoNeAnanasbI95/test-task/internal/service"
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
+	"log/slog"
+)
+
+type Handler struct {
+	log      *slog.Logger
+	services *service.Service
+}
+
+func NewHandler(log *slog.Logger, s *service.Service) *Handler {
+	return &Handler{
+		log:      log,
+		services: s,
+	}
+}
+
+func (h *Handler) InitRouts() *gin.Engine {
+	router := gin.New()
+	router.Use(h.CORSMiddleware)
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	api := router.Group("/api")
+	{
+		api.GET("/songs", h.GetSongs())
+		api.GET("/songs/:id/lyrics", h.GetSongLyrics())
+		api.DELETE("/songs/:id", h.DeleteSong())
+		api.PUT("/songs/:id", h.UpdateSong())
+		api.POST("/songs", h.AddSong())
+	}
+	return router
+}
--- internal/handler/songs.go
+++ internal/handler/songs.go
@@ -1,1 +1,29 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) GetSongs() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		// TODO сделать пагинацию
+		//group := c.Query("group")
+		//song := c.Query("song")
+		//page := c.DefaultQuery("page", "1")
+		//limit := c.DefaultQuery("limit", "10")
+	}
+}
+
+func (h *Handler) GetSongLyrics() gin.HandlerFunc {
+	panic("implement me")
+}
+
+func (h *Handler) DeleteSong() gin.HandlerFunc {
+	panic("implement me")
+}
+
+func (h *Handler) UpdateSong() gin.HandlerFunc {
+	panic("implement me")
+}
+
+func (h *Handler) AddSong() gin.HandlerFunc {
+	panic("implement me")
+}
