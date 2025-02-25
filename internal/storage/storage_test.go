4ec95da39b7f2093c1e36c31b9fc6dfedf7220c0
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2025-02-26 02:20:10 +0300

    feat: add db connection loading logic

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -1,1 +1,11 @@
 package storage
+
+import (
+	"github.com/EtoNeAnanasbI95/test-task/internal/config"
+	"testing"
+)
+
+func TestMustInitDB(t *testing.T) {
+	cfg := config.MustLoadConfig("../../.env")
+	MustInitDB(cfg.ConnectionString)
+}
