a57e069498e4422e7c32aff6f1146b92b77c7bf3
EtoNeAnanasbI95 <ananasovii.yashik@gmail.com>
2025-02-26 02:19:41 +0300

    feat: init base repo

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,15 @@
 package repository
+
+import (
+	"github.com/jmoiron/sqlx"
+	"log/slog"
+)
+
+// TODO create repository interface
+type Repository struct {
+	log *slog.Logger
+}
+
+func NewRepository(l *slog.Logger, db *sqlx.DB) *Repository {
+	return &Repository{log: l}
+}
